860807dad5bb8d9ed69b59da6f4f726c9cbc18d0
Nhinhoshxhy Desprez <noshdesprez@gmail.com>
2020-02-14 16:28:58 +0000

    printed the current date

---
+++ src/CST_practice.py
@@ -0,0 +1,4 @@
+import datetime
+from datetime import datetime
+d = datetime.now()
+print(f'It is now: ' + d.strftime("%a %d, %B %Y 10:30 %p"))
--- src/models.py
+++ src/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
+from datetime import datetime
 
 db = SQLAlchemy()
 
